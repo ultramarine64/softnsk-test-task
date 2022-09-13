76338f6ff6debe13d3e3820d410642eea1f7aa67
Pavel Gatin <imnotcreative@yandex.ru>
2022-09-14 03:43:29 +0700

    Добавить boilerplate.

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync');
+const sass = require('gulp-sass')(require('sass'));
+const cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
+const rename = require("gulp-rename");
+const htmlmin = require('gulp-htmlmin');
+
+gulp.task('watch', function() {
+    gulp.watch("src/*.html", gulp.parallel('html'));
+    gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel('styles'));
+    gulp.watch("src/js/**/*.js", gulp.parallel('scripts'));
+    gulp.watch("src/images/**/*", gulp.parallel('images'));
+});
+
+gulp.task('server', function() {
+    browserSync({
+        server: {
+            baseDir: "dist"
+        }
+    });
+    gulp.watch("src/*.html").on('change', browserSync.reload);
+});
+
+gulp.task('html', function() {
+    return gulp.src("src/*.html")
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest("dist/"));
+});
+
+gulp.task('styles', function() {
+    return gulp.src("src/sass/**/*.+(scss|sass)")
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(autoprefixer())
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest("dist/css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('scripts', function() {
+    return gulp.src("src/js/**/*.js")
+        .pipe(gulp.dest("dist/js"));
+});
+
+gulp.task('images', function() {
+    return gulp.src("src/images/**/*")
+        .pipe(gulp.dest("dist/images"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', gulp.parallel('watch', 'server', 'html', 'styles', 'scripts', 'images'));
