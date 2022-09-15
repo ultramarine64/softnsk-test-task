ee041537812be979cc3da9094b350affe2962a91
Pavel Gatin <imnotcreative@yandex.ru>
2022-09-16 03:12:46 +0700

    Updates

---
+++ js/script.js
@@ -0,0 +1,59 @@
+enableMenuBtn();
+trackSliderValueChange();
+enableSelectMenu();
+
+function enableMenuBtn() {
+  const menuBtn = document.querySelector('.menu__btn');
+  const menuBtnSpans = document.querySelectorAll('.menu__btn-span');
+  const menu = document.querySelector('.menu');
+
+  menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('menu_active');
+    for (span of menuBtnSpans) {
+      span.classList.toggle('menu__btn-span_active');
+    }
+  });
+}
+
+function trackSliderValueChange() {
+  const orderFormSliderValue = document.querySelector('.order-making .range-slider__value');
+  const orderFormSlider = document.querySelector('.order-making .range-slider__input');
+
+  orderFormSlider.addEventListener('input', () => {
+    orderFormSliderValue.textContent = `${orderFormSlider.value} %`;
+  });
+}
+
+function enableSelectMenu() {
+  const select = document.querySelector(".select-menu__header");
+  const optionsContainer = document.querySelector(".select-menu__options-container");
+  const optionsList = document.querySelector(".select-menu__options-list");
+  const optionsLabels = document.querySelectorAll(".select-menu__option-label");
+  const options = document.querySelectorAll(".select-menu__option");
+
+  select.addEventListener("click", () => {
+    optionsContainer.classList.toggle("select-menu__options-container_active");
+    select.classList.toggle("select-menu__header_active");
+  });
+
+  options.forEach((option) => {
+    option.addEventListener("input", () => {
+      optionsLabels.forEach((optionLabel) => {
+        optionLabel.classList.remove('selected')
+      });
+      parentLabel = option.parentElement;
+      select.querySelector(".select-menu__header-text").textContent = parentLabel.textContent;
+      parentLabel.classList.add("selected");
+      optionsContainer.classList.toggle("select-menu__options-container_active");
+      select.classList.toggle("select-menu__header_active");
+    });
+  });
+
+  window.addEventListener('click', function(e){
+    if (!document.querySelector('.select-menu').contains(e.target) &&
+        optionsContainer.classList.contains("select-menu__options-container_active")){
+      optionsContainer.classList.toggle("select-menu__options-container_active");
+      select.classList.toggle("select-menu__header_active");
+    }
+  });
+}
